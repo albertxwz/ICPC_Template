783ecc6bc28734584549bece2fa54a5d0e5b4ed2
albertxwz <1017981912@qq.com>
2021-02-01 23:49:04 +0800

    Update KMP.cpp

--- codehub/string/KMP.cpp
+++ codehub/string/KMP.cpp
@@ -1,9 +1,8 @@
 int f[maxn];
 
-void getfail(char *P, int *f) {
+void getfail(char *P, int *f, int m) {
 	f[0] = 0;
 	f[1] = 0;
-	int m = strlen(P);
 	for (int i = 1; i < m; ++i) {
 		int j = f[i];
 		while (j && P[i] != P[j]) j = f[j];
@@ -12,9 +11,8 @@ void getfail(char *P, int *f) {
 	}
 }
 
-int find(char *T, char *P, int *f) {
+int find(char *T, char *P, int *f, int n, int m) {
 	int res = 0;
-	int n = strlen(T), m = strlen(P);
 	getfail(P, f);
 	for (int i = 0, j = 0; i < n; ++i) {
 		while (j && P[j] != T[i]) j = f[j];
