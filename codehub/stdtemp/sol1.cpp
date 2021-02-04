3efe9275cd2cdf1987a80a553933cb03dc6ac85f
albertxwz <1017981912@qq.com>
2021-02-04 15:12:31 +0800

    Update sol1.cpp

--- codehub/stdtemp/sol1.cpp
+++ codehub/stdtemp/sol1.cpp
@@ -12,7 +12,19 @@ const int dy[4] = {0, 1, 0, -1};
 
 typedef long long LL;
 
+template<class T>
+inline void read(T &x)
+{
+    x=0;int f=0;char ch=getchar();
+    while(ch<'0'||ch>'9')  {f|=(ch=='-');ch=getchar();}
+    while(ch>='0'&&ch<='9'){x=(x<<1)+(x<<3)+(ch^48);ch=getchar();}
+    x=f?-x:x;
+    return;
+}
+
 int main() {
     // freopen("my.txt", "w", stdout);
+	ios::sync_with_stdio(false); // do not use endl
+	cin.tie(0); // cannot use cout/printf or cin/scanf at the same time.
 	return 0;
 }
