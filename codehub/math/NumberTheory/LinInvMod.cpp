c5a6679aa204bbc3502504153c26942075fdf6c9
albertxwz <1017981912@qq.com>
2021-07-09 15:15:00 +0800

    Delete codehub directory

--- codehub/math/NumberTheory/LinInvMod.cpp
+++
@@ -1,6 +0,0 @@
-const int maxn = 2e5 + 5;
-const int modu = 1e9 + 7;
-long long inv[maxn]; // k在模modu的意义下的逆元是inv[k]
-inv[1] = 1;
-for (int i = 2; i < maxn; ++i)
-    inv[i] = (modu - (modu/i))*inv[modu%i]%modu;
